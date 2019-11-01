82fe69eb9658678d6888f7ce4f4be590c5e42d68
jsmask <2674800533@qq.com>
2019-11-01 11:30:22 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
 const { override, fixBabelImports, addLessLoader,addWebpackAlias,addDecoratorsLegacy } = require('customize-cra');
 const path = require("path");
 function resolve(dir) {
-  return path.join(__dirname, '.', dir)
+  return path.join(__dirname, '.', dir);
 }
 module.exports = override(
   fixBabelImports('import', {
